8789f1a417087c4bfda166c6e4f996b98d79450d
Gabriel Badea <gabi.badea313@gmail.com>
2022-06-28 16:14:44 +0300

    Update version

--- src/interface.hpp
+++ src/interface.hpp
@@ -20,7 +20,7 @@ private:
 
     std::vector<std::string> m_packPaths;
     std::vector<std::string> m_packNames;
-    std::string m_programVersion = "1.1.0";
+    std::string m_programVersion = "1.1.0-dev";
 
 public:
     // Initializes the interface and starts the first-time setup if no config file is detected
