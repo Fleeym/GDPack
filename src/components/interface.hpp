aeb6cd8d3d70f15070239aa5cf92bc1ab0c89d28
Fleeym <gabi.badea313@gmail.com>
2022-11-09 21:59:21 +0200

    Update version

--- src/components/interface.hpp
+++ src/components/interface.hpp
@@ -25,7 +25,7 @@ class Interface {
 
     std::vector<std::string> m_packPaths;
     std::vector<std::string> m_packNames;
-    std::string m_programVersion = "2.0.0";
+    const std::string m_programVersion = "v2.1.0";
 
   public:
     // Initializes the interface and starts the first-time setup if no config
